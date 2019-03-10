496fc931c9c52da47fef379b12a7760d47923813
Paweł Zmarzły <pawo2500@gmail.com>
2019-03-10 13:44:01 +0100

    removed `sends`

--- src/lib.rs
+++ src/lib.rs
@@ -11,23 +11,16 @@ impl JoinableHandle for StdJoinHandle<()> {
     }
 }
 
-pub struct Handle<T: JoinableHandle, TX, RX> {
+pub struct Handle<T: JoinableHandle, TX> {
     pub join_handle: T,
     pub tx: std::sync::mpsc::Sender<TX>,
-    pub rx: std::sync::mpsc::Receiver<RX>,
 }
 
-impl<T: JoinableHandle, TX, RX> Handle<T, TX, RX> {
+impl<T: JoinableHandle, TX> Handle<T, TX> {
     pub fn join(self) {
         self.join_handle.join();
     }
     pub fn send(&mut self, msg: TX) {
         self.tx.send(msg).unwrap();
-    }
-    pub fn recv(&mut self) -> RX {
-        self.rx.recv().unwrap()
-    }
-    pub fn try_recv(&mut self) -> Result<RX, std::sync::mpsc::TryRecvError> {
-        self.rx.try_recv()
     }
 }
